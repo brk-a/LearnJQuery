9fc47273ca6bd9daff91e8f74301c9bdd71a1ddc
brk-a <fnjakai@outlook.com>
2022-08-26 10:08:27 +0300

    selectors in jQ

---
+++ 0x00-LearnJQuery/scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    alert('jquery loaded');
+});
+
+$("h3").css({border: "3px solid blue"});
+
+$(".wrapper").css({border: "3px solid red"});
+
+$("#clients").css({border: "3px solid yellow"});
