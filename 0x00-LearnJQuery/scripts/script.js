c7235db8212219640296ae39755977a51b6feb1b
brk-a <fnjakai@outlook.com>
2022-08-29 12:22:53 +0300

    traversing the DOM and chaining

--- 0x00-LearnJQuery/scripts/script.js
+++ 0x00-LearnJQuery/scripts/script.js
@@ -22,3 +22,17 @@ $("#contact :last-child").css({border: "3px solid blue"});
 //re-call: JS is zero-based and 0, in Js, is even
 $("header nav li:even").css({border: "3px solid grey"});
 $("header nav li:odd").css({border: "3px solid purple"});
+
+// traverse the DOM
+$("#contact-methods").next().css({border: "5px solid purple"});
+$("#social-nav").prev().css({border: "5px solid grey"});
+$(".banner-title").parent().css({border: "5px solid purple"});
+$(".banner-title").parents().css({border: "5px solid blue"});
+$("#social-nav").children().css({border: "5px solid green"});
+$("#contact").find(".facebook").css({border: "3px solid pink"});
+$("#social-nav").closest("wrapper").css({border: "5px solid red"});
+
+//chaining proper
+$("#contact-methods").css({border: "7px solid red"})
+    .next().css({border: "7px solid green"})
+    .closest("section").css({border: "7px solid blue"});
