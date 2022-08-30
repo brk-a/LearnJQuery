58bd526c7a2266e4e405342d2e13fedf8fecf658
brk-a <fnjakai@outlook.com>
2022-08-30 12:46:26 +0300

    adding content, wrapping and unwrapping

--- 0x00-LearnJQuery/scripts/script.js
+++ 0x00-LearnJQuery/scripts/script.js
@@ -36,3 +36,54 @@ $("#social-nav").closest("wrapper").css({border: "5px solid red"});
 $("#contact-methods").css({border: "7px solid red"})
     .next().css({border: "7px solid green"})
     .closest("section").css({border: "7px solid blue"});
+
+//adding content
+//method 1
+var tweet = '<div style="margin: 20px 0; padding: 10px; background: None">\
+    <p>This is sample text for the sample tweet</p>\
+    </div>'
+
+//append...to the bottom of the element seleted
+$('#tweets div').append(tweet);
+
+//prepend...to the beginning of the element selected
+$('#tweets div').prepend(tweet);
+
+//before...the element selected
+$('#tweets div p').before(tweet);
+
+//after...the element selected
+$('#tweets div p').after(tweet);
+
+//html...add a html tag in the element selected
+$('#tweets div').html(tweet);
+
+//text... add text to the element selected
+$('#tweets div p').text(tweet);
+
+//wrap and unwrap
+$("section").wrap('<div class="section">')
+    .css({border: "5px solid grey"});
+
+$("section").unwrap()
+    .css({border: '5px solid pink'});
+
+$("section").wrapAll('<div class="section-all">')
+    .css({border: "5px solid purple"}); //css is applied to sections, not div
+
+var wrapper = "<div class='wrapper-all'>";
+var button = $(".button");
+var wrapped = true;
+
+button[0].onclick = () => {
+    if (wrapped){
+        $("section").unwrap();
+        wrapped = false;
+        button.text('wrap');
+    } else{
+        $("section").wrapAll(wrapper);
+        wrapped = true;
+        button.text('unwrap');
+
+    }
+};
